f6893785dbbff23f521c401958d003c77fcb1716
yashadaparab2025@gmail.com <yashadaparab2025@gmail.com>
2025-01-12 16:48:51 +0530

    Validate CICD

--- src/test/java/YashadaParabAutomation/standalone/StandaloneTest.java
+++ src/test/java/YashadaParabAutomation/standalone/StandaloneTest.java
@@ -21,6 +21,7 @@ public class StandaloneTest extends Initialization {
 		CataloguePage cp=creds.credentials("mere@greysloan.com", "Merelove24");
 		cp.returnList();
 		cp.getWebElement(productName);
+//Validate CI CD
 		
 		CartPage carts=cp.ClickSubmit(productName);
 		
